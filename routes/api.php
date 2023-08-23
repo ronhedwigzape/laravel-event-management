3b54845bcd08a2383385f419f8a933ba32f0e4cd
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-23 20:53:12 +0800

    feat: add api resource routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(['attendee' => 'event']);
