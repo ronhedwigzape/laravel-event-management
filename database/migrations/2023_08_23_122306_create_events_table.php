f44797c19b3db8734d263756f95ea15efe0a4b93
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-23 20:36:22 +0800

    feat: add columns at migrations

--- database/migrations/2023_08_23_122306_create_events_table.php
+++ database/migrations/2023_08_23_122306_create_events_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+
             $table->timestamps();
         });
     }
--- database/migrations/2023_08_23_122626_create_attendees_table.php
+++ database/migrations/2023_08_23_122626_create_attendees_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Event;
 
 return new class extends Migration
 {
@@ -13,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('attendees', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Event::class);
             $table->timestamps();
         });
     }
