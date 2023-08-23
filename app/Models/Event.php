5a687c7ac75100cfbd994fd5678f5e7414e9cae2
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-23 20:44:13 +0800

    feat: add relationships at models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function attendees(): HasMany
+    {
+        return $this->hasMany(Attendee::class);
+    }
 }
