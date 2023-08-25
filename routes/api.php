d6581e5c5d607e8c98293b2477e59b2931f30a2c
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-25 16:42:43 +0800

    feat: setup authentication with laravel sanctum
    Create login method for AuthController
    
    Artisan commands used:
    
    - `php artisan make:controller Api/AuthController`

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ use App\Http\Controllers\Api\AttendeeController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
