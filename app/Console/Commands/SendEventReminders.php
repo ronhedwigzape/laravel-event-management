f3fe4ede611f92913b27cdc3e642b336868c3ac2
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-27 01:07:37 +0800

    feat: add a email notification
    - Sends an email notification to user's email
    
    Artisan command used:
    
    - `php artisan make:notification EventReminderNotification`

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -38,7 +39,9 @@ class SendEventReminders extends Command
 
         $events->each(
             fn($event) => $event->attendees->each(
-                fn($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+                fn($attendee) => $attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
             )
         );
 
